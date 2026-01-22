ccb5e1d0c754a86d61e0826f4249a4dc3b0400a8
Shlykovpv <Shlykovpv@kamaz.ru>
2026-01-22 17:32:26 +0300

    controllers, module, model

---
+++ src/product/product.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { FindProductDTO } from './dto/find-product.dto';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('product')
+export class ProductController {
+    constructor( private readonly configService: ConfigService){}
+
+    @Post('create')
+    async create(@Body() dto: Omit<ProductController, '_id'>){
+        this.configService.get('TEST') // пример получения переменной env
+
+    }
+
+    @Get(':id')
+    async get(@Param('id') id: string){
+
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id:string){
+
+    }
+
+    @Patch(':id')
+    async patch(@Param('id') id:string, @Body() dto: ProductController){
+        
+    }
+
+    @HttpCode(200)
+    @Post()
+    async find(@Body() dto: FindProductDTO){
+        
+    }
+
+}
